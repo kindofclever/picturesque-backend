53c76dd498f672b0d58dc4e9e5f9fd8a4d6ba9d2
Sandra Gufler <97165184+kindofclever@users.noreply.github.com>
2022-11-08 09:56:42 +0100

    initial commit

---
+++ schemas/card.js
@@ -0,0 +1,32 @@
+export default {
+  name: 'card',
+  title: 'Card',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Titel',
+      type: 'string',
+    },
+    {
+      name: 'about',
+      title: 'About',
+      type: 'string',
+    },
+    {
+      name: 'destinationUrl',
+      title: 'DestinationURL',
+      type: 'url',
+    },
+    {
+      name: 'category',
+      title: 'Category',
+      type: 'string',
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'string',
+    },
+  ],
+}
